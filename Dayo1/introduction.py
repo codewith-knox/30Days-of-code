9098a77c5fe2a3d20c0539be1b2d870c904b6403
shahil gupta <sahilgupta3839@gmail.com>
2026-09-07 07:46:47 +0530

    version1

---
+++ Dayo1/introduction.py
@@ -0,0 +1,26 @@
+# print() is use to display output on the console
+
+
+print("Row, row, row your boat,\n" + "Gently down the stream.\n" + "Merrily, merrily, merrily, merrily,\n" + "Life is but a dream.")  
+print(type("python \n"+"3"))  #type() is used to check data types
+print(3)
+print("@")
+
+
+name= input("What is your name? ")               #name,email,nickname are varaibles that store data and use in future
+email = input("What is your email address? ")
+nickname = input("What is your nickname? ")
+
+print("Let's make sure we got this right")
+print("Your name: " + name)
+print("Your email address: " + email)           # '+' is used to concatenate two string
+print("Your nickname: " + nickname)
+
+part1 = input("The 1st part: ")
+part2 = input("The 2nd part: ")
+part3 = input("The 3rd part: ")
+print(part1 + "-"+part2+"-"+part3)
+
+num =3
+ver="3.14@"
+print(f"python{num}\nversion: {ver}")
