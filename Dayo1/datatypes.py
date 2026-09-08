01214c517378af7199343e44799cd5c628cb0040
shahil gupta <sahilgupta3839@gmail.com>
2026-09-08 16:07:05 +0530

    updated

---
+++ Dayo1/datatypes.py
@@ -0,0 +1,28 @@
+# INTEGERS
+
+age = 24
+print(age)
+
+# Both are different things one strings and other is integers 
+print(100*2)    #genreal arthemetic opertion 
+print("100"*2)    #string multiplicatons 
+
+# when im tring to add string and integers
+# print("100"+100)   #Type error
+
+# print two diffrent data types
+result = 10 * 25
+print("The result is "+ str(result))   #M1 type conversion
+print("The result is ", result)        #M2 use of comma to seprate two data types
+print(f"The result is {result}")        #M3 using f-string
+
+
+name="Aryan"
+age =18
+height =5.5
+print(name,age,height)
+# Resource code
+name = "Mark"
+age = 37
+city = "Palo Alto"
+print(f"Hi {name}, you are {age} years old. You live in {city}.")
--- Dayo1/variables.py
+++ Dayo1/variables.py
@@ -1,6 +1,65 @@
 # varaibles stores data in the memory
 # var = val     in left side there is varname and right side its value
+# Changing the variable:
+var="var1"
+var="var2"
+var=var+"\nchanges" # when u assigned new value to the variables old assigned value is replaced with new one
+print(var)
 
-var1="val1"
-var2="val2"
-print(var1+"\n"+var2)
+name = "Tim Tester"
+age = 20
+skill1 = "python"
+level1 = "beginner"
+skill2 = "java"
+level2 = "veteran"
+skill3 = "programming"
+level3 = "semiprofessional"
+lower = 2000
+upper = 3000
+
+print(f"my name is {name}, I am {age} years old\n")
+print("my skills are")
+print(f'- {skill1} ({level1})')
+print(f'- {skill2} ({level2})')
+print(f'- {skill3} ({level3})\n')
+print(f"I am looking for a job with a salary of {lower}-{upper} euros per month")
+
+
+
+
+# Book solution.  
+
+name = "Tim Tester"
+age = 20
+skill1 = "python"
+level1 = "beginner"
+skill2 = "java"
+level2 = "veteran"
+skill3 = "programming"
+level3 = "semiprofessional"
+lower = 2000
+upper = 3000
+ 
+print(f"my name is {name}, I am {age} years old")
+print("")
+print("my skills are")
+print(" -", skill1, "("+level1+")")
+print(" -", skill2, "("+level2+")")
+print(" -", skill3, "("+level3+")")
+print("")
+print(f"I am looking for a job with a salary of {lower}-{upper} euros per month")
+
+# print("- ",skill1, " (", level1, ")")
+# print("- ",skill2, " (", level2, ")")
+# print("- ",skill3, " (", level3, " )")
+
+
+
+
+
+# # print("aryan","gupta")
+
+# # print("-",skill1,)
+# print(f'- {skill1} ({level1})')
+
+# print(" (",skill1,")")
