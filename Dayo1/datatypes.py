0b9d2f0047827e013095b601ee14f55b5820a24b
shahil gupta <sahilgupta3839@gmail.com>
2026-09-09 07:20:27 +0530

    updated

--- Dayo1/datatypes.py
+++ Dayo1/datatypes.py
@@ -1,28 +1,57 @@
 # INTEGERS
 
-age = 24
-print(age)
+# age = 24
+# print(age)
 
-# Both are different things one strings and other is integers 
-print(100*2)    #genreal arthemetic opertion 
-print("100"*2)    #string multiplicatons 
+# # Both are different things one strings and other is integers 
+# print(100*2)    #genreal arthemetic opertion 
+# print("100"*2)    #string multiplicatons 
 
-# when im tring to add string and integers
-# print("100"+100)   #Type error
+# # when im tring to add string and integers
+# # print("100"+100)   #Type error
 
-# print two diffrent data types
-result = 10 * 25
-print("The result is "+ str(result))   #M1 type conversion
-print("The result is ", result)        #M2 use of comma to seprate two data types
-print(f"The result is {result}")        #M3 using f-string
+# # print two diffrent data types
+# result = 10 * 25
+# print("The result is "+ str(result))   #M1 type conversion
+# print("The result is ", result)        #M2 use of comma to seprate two data types
+# print(f"The result is {result}")        #M3 using f-string
 
 
-name="Aryan"
-age =18
-height =5.5
-print(name,age,height)
-# Resource code
-name = "Mark"
-age = 37
-city = "Palo Alto"
-print(f"Hi {name}, you are {age} years old. You live in {city}.")
+# name="Aryan"
+# age =18
+# height =5.5
+# print(name,age,height)
+# # Resource code
+# name = "Mark"
+# age = 37
+# city = "Palo Alto"
+# print(f"Hi {name}, you are {age} years old. You live in {city}.")
+
+# Floating point numbers
+
+number1 = 2.5
+number2 = -1.25
+number3 = 3.62
+
+mean = (number1 + number2 + number3) / 3
+print(f"Mean: {mean}")
+# print(mean)
+
+# course problem
+
+x,y=27,15
+print(f"{x} + {y} = {x+y}")
+print(f"27-15 = {x-y}")
+print(f"27+*5 = {x*y}")
+print(f"27/15 = {x/y}")
+
+# book solution
+x = 27
+y = 15
+ 
+print(f"{x} + {y} = {x+y}")
+print(f"{x} - {y} = {x-y}")
+print(f"{x} * {y} = {x*y}")
+# Printing can also be made as follows:
+print(x, "/", y, "=", (x / y))
+ 
