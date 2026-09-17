524eb41368b578ad34be8c26e01ccc1216e382fe
shahil gupta <sahilgupta3839@gmail.com>
2026-09-17 19:58:55 +0530

    learn

--- Day03/Exercise_Day03/loyalitybonus.py
+++ Day03/Exercise_Day03/loyalitybonus.py
@@ -22,12 +22,26 @@
 
 # Fix the program
 points = int(input("How many points are on your card? "))
+
+
 if points < 100:
-    points *= 1.1
-    print("Your bonus is 10 %")
+    points += 0.1*points
+    print("Your bonus is 10%")
 
-if points > 100:
-    points *= 1.15
-    print("Your bonus is 15 %")
+else:
+    points += 0.15*points
+    print("Your bonus is 15%")
 
-print("You now have", points, "points")
+print("You now have",points,"points") 
+
+
+
+# if points < 100:
+#     points *= 1.1
+#     print("Your bonus is 10 %")
+
+# if points > 100:
+#     points *= 1.15
+#     print("Your bonus is 15 %")
+
+# print("You now have", points, "points")
